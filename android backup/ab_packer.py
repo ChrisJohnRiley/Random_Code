0668901e5354a9c08c6b0840618c8aea88397e6f
wrzrly <reg@c22.cc>
2013-06-05 14:47:44 +0200

    Added overwrite function Detect ADB issues Variable cleanup VERBOSE --> TODO

--- android backup/ab_packer.py
+++ android backup/ab_packer.py
@@ -18,17 +18,20 @@ import string
 def main():
 
 	usage = "\n%prog [options] arguments"
-	version = "v1.0 - @ChrisJohnRiley"
+	version = "v1.1 - @ChrisJohnRiley"
 	logo(version)
 
 	parser = optparse.OptionParser(usage=usage, version=version)
 
 	parser.add_option("-d", "--directory", dest="directory", help="Directory containing apps folder for repacking")
-	parser.add_option("-b", "--backfile", dest="backfile", help="Resulting Android Backup filename")
+	parser.add_option("-b", "--backfile", dest="backfile", help="Resulting Android Backup filename (NEW)")
 	parser.add_option("-l", "--list", dest="list", help="List created from original backup")
 	parser.add_option("-o", "--original", dest="original", help="Original Android Backup filename")
 	parser.add_option("-r", "--restore", dest="restore", help="Restore to device on completion", action="store_true")
+	parser.add_option("-v", "--verbose", dest="verbose", help="Verbose output", action="store_true")
+	parser.add_option("-x", "--overwrite", dest="overwrite", help="Overwrite files/directories", action="store_true")
 
+	global opts
 	(opts, args) = parser.parse_args()
 
 	if opts.directory and opts.backfile:
@@ -45,20 +48,25 @@ def main():
 		print("\n")
 		sys.exit(0)
 
-	adbrestore = setup(opts.backfile)
-	checks(opts.backfile, opts.directory)
+	if opts.overwrite:
+		print("\n [!] Overwriting enabled -\n")
+		print("\t - existing backup data will be overwritten")
+		print("\t - existing files within the unpack directory will be overwritten\n")
+
+	setup()
+	checks()
 
 	# take list from user supplied file or recreate from original ab
 	if opts.list:
-		listfile = opts.list	
+		opts.listfile = opts.list
 	else:
-		randfilename = decode(opts.original)
-		listfile = create_list(opts.original, randfilename)
+		decode()
+		create_list()
 
-	pack(opts.directory, opts.backfile, listfile, opts.original)
+	pack()
 
 	if opts.restore:
-		restore(adbrestore)
+		restore()
 
 	print("\n [ ] Script completed...\n\n")
 
@@ -73,23 +81,23 @@ def logo(version):
          ______| |                             
         |_\__\_|_|       %s''') % version
 
-def setup(backfile):
+def setup():
 	
 	# setup variables for the script
-	adbrestore = 'adb restore ' + backfile
+	opts.adbrestore = 'adb restore ' + opts.backfile
 
-	return (adbrestore)
+def checks():
 
-def checks(backfile, directory):
+	if not opts.overwrite:
 
-	# check if file and/or directory exist already
-	if os.path.exists(backfile):
-		print(" [X] Restore file of the same name: %s already exists") % backfile
-		sys.exit(1)
-
-	if not os.path.exists(directory):
-		print(" [X] Source directory: %s does not exist") % directory
-		sys.exit(1)
+		# check if file and/or directory exist already
+		if os.path.exists(opts.backfile):
+			print(" [X] Restore file of the same name: %s already exists\n") % opts.backfile
+			sys.exit(1)
+	
+		if not os.path.exists(opts.directory):
+			print(" [X] Source directory: %s does not exist\n") % opts.directory
+			sys.exit(1)
 
 	# check openssl zlib support
 	child = pexpect.spawn ('openssl -help')
@@ -109,71 +117,73 @@ def checks(backfile, directory):
 		print(" [X] Please install star and try again")
 		sys.exit(1)
 
-def restore(adbrestore):
+def restore():
 
 	# restore application over adb
-	print(" [ ] Running Android Restore: %s") % adbrestore
+	print(" [ ] Running Android Restore: %s") % opts.adbrestore
 
-	print("\n [>] Accept restore prompt on Android device to complete restore process...")
-	child = pexpect.spawn (adbrestore)
+	print("\n [>] Accept restore prompt on Android device to complete restore process...\n")
+	child = pexpect.spawn (opts.adbrestore)
+	# check if adb errored out - can't use expect due to stderr?
 	for line in child:
-		print line,
+		if "unable to connect for backup" in line:
+			print(" [X] ADB is unable to detect an Android device - cancelling restore\n")
+			sys.exit(1)
+		else:
+			print line,
 
-def decode(original):
+def decode():
 
 	# decode Android Backup using dd and openssl zlib
-	print(" [ ] Unpacking original Android Backup (%s) to create list") % original
+	print(" [ ] Unpacking original Android Backup (%s) to create list") % opts.original
 
-	randfilename = ''.join(random.choice(string.lowercase) for i in range(6))
+	opts.randfilename = ''.join(random.choice(string.lowercase) for i in range(6))
 	# convert ab to tar
-	child = pexpect.spawn ('/bin/bash -c "dd if=' + original + ' bs=24 skip=1 | openssl zlib -d > ' + randfilename + '.tar"')
+	child = pexpect.spawn ('/bin/bash -c "dd if=' + opts.original + ' bs=24 skip=1 | openssl zlib -d > ' + opts.randfilename + '.tar"')
 	i = child.expect('records out')
 
 	if i==1:
-		print(" [X] Unable to decompress original Android Backup file (%s)") % original
+		print(" [X] Unable to decompress original Android Backup file (%s)") % opts.original
 		sys.exit(1)
 
-	if not os.path.exists(randfilename + '.tar'):
-		print(" [X] Unable to create TAR file (%s)") % (randfilename + '.tar')
+	if not os.path.exists(opts.randfilename + '.tar'):
+		print(" [X] Unable to create TAR file (%s)") % (opts.randfilename + '.tar')
 		sys.exit(1)
 
-	return randfilename
-
-def create_list(original, randfilename):
+def create_list():
 
 	# create list from TAR file (for use in repacking)
-	print(" [ ] Creating filelist from TAR file")
-	child = pexpect.spawn ('/bin/bash -c "tar -tf ' + randfilename + '.tar > ' + original + '.list"')
+	print(" [ ] Creating filelist from TAR file (%s)") % (opts.original + '.tar.list')
+	child = pexpect.spawn ('/bin/bash -c "tar -tf ' + opts.randfilename + '.tar > ' + opts.original + '.tar.list"')
 	for line in child:	
 		print line,
-	listfile = original + '.list'
+	opts.listfile = opts.original + '.tar.list'
 
 	# cleanup temp file
-	os.remove(randfilename + '.tar')
+	os.remove(opts.randfilename + '.tar')
 
-	return listfile
-
-def pack(directory, backfile, listfile, original):
+def pack():
 
 	# create tar from source directory
-	randfilename = ''.join(random.choice(string.lowercase) for i in range(6))
-	print("\n [>] Creating TAR file from source directory ./%s\n") % directory
-	child = pexpect.spawn ('star -c -v -f ./' + randfilename + '.tar -no-dirslash list=' + listfile + ' -C ' + directory)
+	opts.randfilename = ''.join(random.choice(string.lowercase) for i in range(6))
+	print("\n [>] Creating TAR file from source directory ./%s\n") % opts.directory
+	child = pexpect.spawn ('star -c -v -f ./' + opts.randfilename + '.tar -no-dirslash list=' + opts.listfile + ' -C ' + opts.directory)
 	for line in child:
 		if line.startswith('a '):	
-			print(" [] Added: %s") % ('./' + directory + '/' + line[2:]),
+			print(" [] Added: %s") % ('./' + opts.directory + '/' + line[2:]),
 
 	print("\n [>] Creating Android Backup from source directory")
-	child = pexpect.spawn ('/bin/bash -c "dd if=' + original + ' bs=24 count=1 of=' + backfile + '; openssl zlib -in ' + randfilename + '.tar >> ' + backfile + '"')
+	child = pexpect.spawn ('/bin/bash -c "dd if=' + opts.original + ' bs=24 count=1 of=' + opts.backfile + '; openssl zlib -in ' + opts.randfilename + '.tar >> ' + opts.backfile + '"')
 	i = child.expect('records out')
 
 	if i==1:
-		print(" [X] Unable to compress Android Backup file (%s)") % backfile
+		print(" [X] Unable to compress Android Backup file (%s)") % opts.backfile
 		sys.exit(1)
+	else:
+		print(" [ ] Android Backup file (%s) created") % opts.backfile
 
 	# cleanup temp file
-	os.remove(randfilename + '.tar')
+	os.remove(opts.randfilename + '.tar')
 
 if __name__ == "__main__":
    main()
-
